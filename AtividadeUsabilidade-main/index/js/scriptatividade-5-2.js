745608f37e6ff6f0c3e706810f1caa72b4395855
Thiago Carvalho <100310352+ImThigas@users.noreply.github.com>
2023-04-26 16:09:20 -0300

    Add files via upload

---
+++ AtividadeUsabilidade-main/index/js/scriptatividade-5-2.js
@@ -0,0 +1,8 @@
+function avancar(){
+    var login = document.getElementById("pn");
+    if(login.value){
+        window.location.href="Atividade-5-2-2.html";
+    }else{
+
+    }
+}
---
+++ AtividadeUsabilidade-main/index/js/scriptatividade42.js
@@ -0,0 +1,36 @@
+function anaweb(){
+    alert("Disciplina: Web\nAluno: Ana\nP1: 29\nP2: 17\nTotal: 46");
+}
+function evaweb(){
+    alert("Disciplina: Web\nAluno: Eva\nP1: 27\nP2: 17\nTotal: 44");
+}
+function gilweb(){
+    alert("Disciplina: Web\nAluno: Gil\nP1: 28\nP2: 15\nTotal: 43");
+}
+function iseweb(){
+    alert("Disciplina: Web\nAluno: Ise\nP1: 25\nP2: 15\nTotal: 40");
+}
+function iveweb(){
+    alert("Disciplina: Web\nAluno: Ive\nP1: 26\nP2: 20\nTotal: 46");
+}
+function mediaweb(){
+    alert("Média da Disciplina Web: 43,8");
+}
+function analog(){
+    alert("Disciplina: Lógica de Programação\nAluno: Ana\nP1: 29\nP2: 17\nTotal: 46");
+}
+function evalog(){
+    alert("Disciplina: Lógica de Programação\nAluno: Eva\nP1: 27\nTotal: 44");
+}
+function gillog(){
+    alert("Disciplina: Lógica de Programação\nAluno: Gil\nP1: 28\nP2: 15\nTotal: 43");
+}
+function iselog(){
+    alert("Disciplina: Lógica de Programação\nAluno: Ise\nP1: 25\nTotal: 40");
+}
+function ivelog(){
+    alert("Disciplina: Lógica de Programação\nAluno: Ive\nP1: 26\nP2: 20\nTotal: 46");
+}
+function medialog(){
+    alert("Média da Disciplina Lógica de Programação: 43,8");
+}
